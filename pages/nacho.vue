816d39996389bfdb0ece7af1c792074a7caab564
Sebastian <odm_sd@hotmail.com>
2021-09-06 13:31:11 -0500

    cambio: modificacion de compoenete nacho

--- pages/nacho.vue
+++ pages/nacho.vue
@@ -1,11 +1,46 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Cualquier cosa
-        </v-card-title>
-      </v-card>
+    <v-col cols="12" sm="12" md="12">
+      <v-footer
+        color="primary lighten-1"
+        padless
+      >
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+          >
+            {{ link }}
+          </v-btn>
+          <v-col
+            class="primary lighten-2 py-4 text-center white--text"
+            cols="12"
+          >
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
     </v-col>
   </v-row>
 </template>
+<script>
+export default {
+  data: () => ({
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us',
+    ],
+  }),
+}
+</script>
