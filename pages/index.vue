5db02e96c406c12734c65c2e53684a59c7114efe
Sebastian <odm_sd@hotmail.com>
2021-09-10 14:42:05 -0500

    fix: prueba de commit

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          Test de Pull Request
+          Test de Pull Request Prueba
         </v-card-title>
         <v-card-text>
           <div>Dentro del directorio <pre>/pages</pre> cree archivos <code>.vue</code> para que se muestren en el <b>Menú</b></div>
